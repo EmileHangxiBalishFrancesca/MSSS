eb241f5d2504831ea1f673bde03834a23c8c4f45
emilecourth <32646776+emilecourth@users.noreply.github.com>
2017-12-16 23:37:33 +0100

    Update outcomes_plotter.m

--- data/outcomes_plotter.m
+++ data/outcomes_plotter.m
@@ -1,8 +1,9 @@
 close all
-
+clear legend_sentence
+%THE END OF THE LOOPS
 N_p_vector = [72];   % number of people
-N_t_vector = [8];  % number of table % should be ven if it is rectangular % 
-tableShape_vector = [1]; % 1 for circle 2 for rectangular
+N_t_vector = [8];  % number of table % should be ven if it is rectangular %
+tableShape_vector = [1:2]; % 1 for circle 2 for rectangular
 dist_f_vector = [0.01:0.3:3.0]; %distance food-food
 number_statistical_attemps = 20;
 
@@ -16,17 +17,17 @@ for i=1:length(N_p_vector)
                 figure(1)
                 hold on
                 errorbar(dist_f_vector,reshape(cost_time_tot_matrix(i,j,k,:),1,[]),reshape(variance_time(i,j,k,:),1,[]))
-                title('time')
+                
                 hold off
                 figure(2)
                 hold on
                 errorbar(dist_f_vector,reshape(cost_f_tot_matrix(i,j,k,:),1,[]),reshape(variance_f(i,j,k,:),1,[]))
-                title('force')
+                
                 hold off
                 figure(3)
                 hold on
                 errorbar(dist_f_vector,reshape(cost_v_tot_matrix(i,j,k,:),1,[]),reshape(variance_v(i,j,k,:),1,[]))
-                title('velocity')
+                
                 hold off
                 
                 string1 = strcat('N_p=',num2str(N_p_vector(i),'%.3d'));
@@ -40,9 +41,22 @@ for i=1:length(N_p_vector)
         end
     end
 %end
+
+clear legend_sentence
+legend_sentence = ['Circular table disposition   ';'Rectangular table disposition'];
+
 figure(1)
-legend(legend_sentence)
+legend(legend_sentence,'Location','northwest')
+title('Cost related to the time')
+xlabel('Distance between food locations')
+ylabel('Velocity cost')
 figure(2)
-legend(legend_sentence)
+legend(legend_sentence,'Location','northwest')
+title('Cost related to the forces')
+xlabel('Distance between food locations')
+ylabel('Velocity cost')
 figure(3)
-legend(legend_sentence)
+legend(legend_sentence,'Location','northwest')
+title('Cost related to the velocity')
+xlabel('Distance between food locations')
+ylabel('Velocity cost')
